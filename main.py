4d22e1a47f1394f95662fe73ca81b4f368db4275
OldGuyProgrammer <jim@olivi.us>
2023-05-16 12:07:09 -0400

    v0.0.1 Beta - Initial commit to repair folder name change

--- main.py
+++ main.py
@@ -614,9 +614,8 @@ def getoneflight(primarykey):
 
     return flight, 201
 
-
 print(f"Starting INDYCAF WARBIRDS: {__name__}")
-if __name__ == "__main__":
+if __name__ == "main":
     port = os.getenv("PORT")
     if port is None:
         print("PORT environment variable missing.\nSetting port to 3000")
